e8bc42299be1d568a825064abff78f8b2f5ae80a
Shiva <sivakumar0993@gmail.com>
2020-01-18 14:42:25 -0500

    heroku

--- public/js/app.js
+++ public/js/app.js
@@ -16,11 +16,10 @@ const m1 = document.querySelector('#message-1');
 const m2  = document.querySelector('#message-2');
 console.log(search.value);
 weatherForm.addEventListener('submit', (e) => {
-    e.preventDefault(); 
-    console.log(search.value);
+    e.preventDefault();
     m1.textContent = '...Loading';
     m2.textContent = '';
-    fetch(`http://localhost:4200/weather?address=${search.value}`).then((res) => {
+    fetch(`/weather?address=${search.value}`).then((res) => {
         res.json().then((data) => {
             console.log(data);
             m1.textContent = data.location;
--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 const viewaPath = path.join(__dirname, '../templates/views');
 const hbsPartialsPath = path.join(__dirname, '../templates/partials');
 
+const port = process.env.PORT || 4200;
  app.set('view engine', 'hbs');
  app.set('views', viewaPath);
 app.use(express.static(publicDirectoryPath));
@@ -92,6 +93,6 @@ app.get('*', (req, res) => {
 //     })
 // })
 
-app.listen(4200, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log('Server is up on port '+ port)
 })
